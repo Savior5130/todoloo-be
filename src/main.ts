081879d1a7070924107ae875666955c2ec9faa22
Philips <philipstan5130@gmail.com>
2024-08-22 08:01:33 +0700

    chore:add cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Todoloo API')
